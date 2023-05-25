45bcbb2ad5650ebbbd76f09ec9f921d60da68f31
tiago <99084072+santa2150@users.noreply.github.com>
2023-05-25 15:56:49 -0500

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartaComponent } from './carta/carta.component';
-import { JulComponent } from './jul/jul.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartaComponent,
-    JulComponent
-  ],
+],
   imports: [
     BrowserModule,
     AppRoutingModule
